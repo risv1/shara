e971969d3ce272abcbb3f36c758795b4c065825b
risv1 <rishi.aangarai@gmail.com>
2024-10-10 20:56:49 +0530

    feat: add reverse proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod proxy;
+
+use std::convert::Infallible;
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::Server;
+
+#[tokio::main]
+async fn main() {
+    let addr = ([127, 0, 0, 1], 8080).into();
+    
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(proxy::reverse_proxy)) });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
 }
---
+++ src/proxy.rs
@@ -0,0 +1,31 @@
+use std::convert::Infallible;
+use hyper::{Body, Client, Request, Response, Uri};
+
+pub async fn reverse_proxy(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let backend_url = "http://localhost:8000";
+
+    let client = Client::new();
+
+    let mut parts = req.uri().clone().into_parts();
+    let new_uri = format!("{}{}", backend_url, parts.path_and_query.unwrap());
+    let uri: Uri = new_uri.parse().unwrap();
+
+    let new_req = Request::builder()
+        .method(req.method())
+        .uri(uri)
+        .body(req.into_body())
+        .unwrap();
+
+    let response = client.request(new_req).await;
+
+    match response {
+        Ok(res) => Ok(res),
+        Err(err) => {
+            eprintln!("Error contacting backend: {:?}", err);
+            Ok(Response::builder()
+                .status(502) 
+                .body(Body::from("Bad Gateway"))
+                .unwrap())
+        }
+    }
+}
